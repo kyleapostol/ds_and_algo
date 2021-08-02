68af9eed1faa3906b48b7df647355a80d2a11f5c
Kyle Apostol <kyleapostol@yahoo.com>
2021-08-01 22:24:22 -0700

    finished mean and median functions

--- main.js
+++ main.js
@@ -42,4 +42,44 @@ const servedOrders = [1, 2, 4, 6, 5, 3];
 // console.log(hasPalindromePermutation("aabcbcd"));
 let sentence = 'Chocolate cake for dinner and pound cake for dessert';
 
-console.log(new WorldCloudData(sentence).wordsToCounts);
+// console.log(new WorldCloudData(sentence).wordsToCounts);
+
+
+//mode, median, range, mean
+
+let arrNum = [9,3,1,8,3,6,5];
+
+function findTheMean(arr){
+// Add all the numbers then divide by the amount of numbers
+    let total = 0;
+
+    for(let i = 0; i < arr.length; i++){
+        total = total + arr[i];
+    }
+
+    return (total/arr.length);
+};
+
+
+function findTheMedian(arr){
+    //Order the set of numbers, the median is the middle number
+
+    let sortedArr = arr.sort((first, second) => first - second);
+    //if the length is even
+    if(sortedArr.length%2 === 0){ 
+        let half = sortedArr.length/2;
+        return (sortedArr[half] + sortedArr[half-1])/2
+    } else {
+    //if the length is odd
+        let half = (sortedArr.length-1)/2
+        return sortedArr[half];
+    }
+}
+
+function findTheRange(arr){
+
+}
+
+function findTheMode(arr){
+
+}
